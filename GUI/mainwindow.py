5016c4a0261b93c63d78c2bc71f582470afb9af8
Qwerty <i@dont.know>
2022-07-18 21:55:11 +0300

    Mainwindow completed

--- GUI/mainwindow.py
+++ GUI/mainwindow.py
@@ -1,6 +1,9 @@
-from PyQt5.QtCore import pyqtSlot
-from PyQt5.QtWidgets import QWidget, QMenu, QTreeView, QMainWindow
+from PyQt5.QtCore import pyqtSlot, QFile, QIODevice
+from PyQt5.QtGui import QKeySequence
+from PyQt5.QtWidgets import QWidget, QMenu, QTreeView, QMainWindow, QFileDialog
 from PyQt5.QtXml import QDomDocument
+
+from Models.dom_model import DomModel
 
 
 class MainWindow(QMainWindow):
@@ -12,8 +15,26 @@ class MainWindow(QMainWindow):
         self.xml_path = ''
         self.view = QTreeView(self)
 
-        file_menu = menuBar.addMenu()
+        file_menu = self.menuBar().addMenu('File')
+        file_menu.addAction('&Open...', MainWindow.open_file, QKeySequence.Open)
+        file_menu.addAction('E&xit', QWidget.close, QKeySequence.Quit)
+
+        self.view.setModel(self.model)
 
     @pyqtSlot()
     def open_file(self):
-        pass
+        file_path: str = QFileDialog.getOpenFileName(self, 'Open File',
+                                                     self.xml_path, 'XML files (*.xml);;HTML files (*.html);;'
+                                                                    'SVG files (*.svg);;User Interface files (*.ui)')
+
+        if file_path != '':
+            file: QFile = QFile(file_path)
+            if file.open(QIODevice.ReadOnly):
+                document: QDomDocument = QDomDocument()
+                if document.setContent(file):
+                    new_model: DomModel = DomModel(document, self)
+                    self.view.setModel(new_model)
+                    del self.model
+                    self.xml_path = file_path
+
+            file.close()
--- main.py
+++ main.py
@@ -2,7 +2,19 @@ import sys
 
 from PyQt5.QtWidgets import QApplication
 
-if __name__ == '__main__':
+from GUI.mainwindow import MainWindow
+
+
+def main():
     app = QApplication(sys.argv)
 
-    mainwindow = None
+    window = MainWindow()
+    window.resize(640, 480)
+    window.show()
+
+    return app.exec_()
+
+
+if __name__ == '__main__':
+    main()
+
